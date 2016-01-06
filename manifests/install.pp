082fee4ac308962219e541f1f1847bd3b6e1c988
Neil Parley <mail@neilparley.co.uk>
2016-01-06 11:27:32 +0000

    Add install.pp to the repo

---
+++ manifests/install.pp
@@ -0,0 +1,15 @@
+class r::install ( $version = 'installed' ) {
+
+  include r::repository
+
+  case $::operatingsystem {
+    'Ubuntu': {
+      Class['r::repository'] ->  Class["apt::update"] -> package {'r-base': ensure => $version, } -> package {'r-base-dev': ensure => $version}
+    }
+    'CentOS': {
+      Class['r::repository'] -> package {'R': ensure => $version, }
+    }
+    default: { fail("Not supported on osfamily ${::operatingsystem}") }
+  }
+
+}
