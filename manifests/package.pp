95e400164d95e35797d6bd74383c78390ce58e5d
Neil Parley <mail@neilparley.co.uk>
2016-01-06 11:30:43 +0000

    Update to work with multi repos when sourcing the R packages. Always fail if the package does not install by calling the library command after the install package command.

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,12 +1,15 @@
-define r::package($r_path = '/usr/bin/R', $repo = 'http://cran.rstudio.com', $dependencies = false) {
+define r::package($r_path = '/usr/bin/R', $repo = ['http://cran.rstudio.com',], $dependencies = false) {
+
+  $repos = join($repo, "\' ,'")
+  include ::r
 
   exec { "install_r_package_${name}":
     command => $dependencies ? {
-      true    => "${r_path} -e \"install.packages('${name}', repos='${repo}', dependencies = TRUE)\"",
-      default => "${r_path} -e \"install.packages('${name}', repos='${repo}', dependencies = FALSE)\""
+      true    => "${r_path} -e \"install.packages('${name}', repos=c('${repos}'), dependencies = TRUE); library(${name})\"",
+      default => "${r_path} -e \"install.packages('${name}', repos=c('${repos}'), dependencies = FALSE); library(${name})\""
     },
     unless  => "${r_path} -q -e '\"${name}\" %in% installed.packages()' | grep 'TRUE'",
-    require => Class['r']
+    require => Class['::r']
   }
 
 }
