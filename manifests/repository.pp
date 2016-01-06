defa4e48c51e628dac2025f86901df441ca095bc
Neil Parley <mail@neilparley.co.uk>
2016-01-06 11:27:58 +0000

    Add repository.pp to the repo

---
+++ manifests/repository.pp
@@ -0,0 +1,24 @@
+class r::repository {
+
+  case $::operatingsystem {
+    'Ubuntu': {
+      include ::apt
+      $codename       = $::lsbdistcodename
+      $os = downcase($::operatingsystem)
+      apt::source { 'r-project':
+              location    => "http://cran.r-project.org/bin/linux/${os}",
+              # the '/' must be present, otherwise repo name is required
+              release     => "${codename}/",
+              repos       => '', # default repo is main (not present at cran)
+              include => { 'src' => false },
+              key => { 'server' => 'keyserver.ubuntu.com', 'id' =>  'E298A3A825C0D65DFD57CBB651716619E084DAB9'},
+              notify      => Class['apt::update'],
+            }
+    }
+    'CentOS': {
+      contain ::epel
+    }
+    default: { fail("Not supported on osfamily ${::operatingsystem}") }
+  }
+
+}
